52c4acd783149961f0bfd2ff468d92ce8e90e3a0
Daya Shrestha <dayashrestha@Dayas-MacBook-Air.local>
2019-02-23 13:23:02 +0100

    Added main menu scene

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Script to manage the main menu. Loads scenes and quits the game.
+/// </summary>
+public class MainMenu : MonoBehaviour
+{
+    public void LoadScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
+
