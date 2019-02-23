1b9b158de7c3a51eb965a195efbfd71551b577b8
Daya Shrestha <dayashrestha@Dayas-MacBook-Air.local>
2019-02-23 16:13:59 +0100

    tried to make ammo slots

---
+++ Assets/Scripts/AmmoManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AmmoManager : MonoBehaviour
+{
+    private static int ammo = 10;
+    public Text uiText;
+
+    public static AmmoManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void DecreaseAmmo(int decrease)
+    {
+        ammo -= decrease;
+        uiText.text = ammo.ToString();
+    }
+}
