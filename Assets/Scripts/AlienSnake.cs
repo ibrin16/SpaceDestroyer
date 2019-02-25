059d310df4cc381e142a113343ea6834c057989a
Daya Shrestha <dayashrestha@Dayas-MacBook-Air.local>
2019-02-25 13:05:45 +0100

    added animating enemy prefabs

---
+++ Assets/Scripts/AlienSnake.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienSnake : Enemies
+{
+    public float framesPerSecond;
+
+    public override void SetHealthAndDamage()
+    {
+        start_health = 3;
+        damage = 10;
+    }
+
+    public override void Movement()
+    {
+        Vector3 offset = new Vector3(0.5f, 0, 0) * Mathf.Sin(Time.time);
+        transform.position = startPosition + offset;
+    }
+
+    public override void Animate()
+    {
+        StartCoroutine(PlayAnimation());
+    }
+
+    IEnumerator PlayAnimation()
+    {
+        while (true)
+        {
+            int currentFrameIndex = 0;
+            while (currentFrameIndex < aframes.Length)
+            {
+                sr.sprite = aframes[currentFrameIndex];
+                yield return new WaitForSeconds(1f / framesPerSecond);
+                currentFrameIndex++;
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/Enemies.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemies : MonoBehaviour
+{
+    protected Rigidbody2D rgbd;
+    protected SpriteRenderer sr;
+    protected Vector3 startPosition;
+
+    protected int start_health;
+    protected int damage;
+    public bool dead = false;
+    public Sprite[] aframes;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+        sr = GetComponent<SpriteRenderer>();
+        rgbd = GetComponent<Rigidbody2D>();
+        Animate();
+        SetHealthAndDamage();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        if (dead)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public abstract void Movement();
+
+    public abstract void Animate();
+
+    public abstract void SetHealthAndDamage();
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") || collision.CompareTag("Pistol"))
+        {
+            PlayerInteraction.instance.Hit(damage);
+            print("here1");
+        }
+        if (collision.CompareTag("Laser"))
+        {
+            Hit(1);
+            Destroy(collision.gameObject);
+        }
+        if (collision.CompareTag("Ball"))
+        {
+            Hit(2);
+            Destroy(collision.gameObject);
+
+        }
+        if (collision.CompareTag("RPG"))
+        {
+            Hit(5);
+            Destroy(collision.gameObject);
+
+        }
+    }
+
+    public void Hit(int damage)
+    {
+        start_health -= damage;
+        if (start_health <= 0)
+        {
+            dead = true;
+            print("enemy death");
+        }
+    }
+
+}
