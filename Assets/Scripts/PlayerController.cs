d2b691e29b1ebce86e621da586bf8fb8cba71047
Daya Shrestha <dayashrestha@Dayas-MacBook-Air.local>
2019-02-26 21:25:13 +0100

    Revert "Revert "screen blinks when player touches snake etc""
    This reverts commit 46aa294dc9681a22182190617164e28900d6de2c.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public static PlayerController instance;
     public float speed = 5;
-    private SpriteRenderer sp;
+    public SpriteRenderer sp;
     public Sprite[] sprites;
     private Rigidbody2D rgbd;
     public float animationTime;
