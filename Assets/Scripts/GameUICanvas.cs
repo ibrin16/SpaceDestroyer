5355a7792eb480eb52467a97758410c96335c004
Daya Shrestha <dayashrestha@8000D-0560.DisStu.local>
2019-03-11 11:53:31 +0100

    should be fixed now

---
+++ Assets/Scripts/GameUICanvas.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameUICanvas : MonoBehaviour
+{
+
+    public static GameUICanvas instance;
+
+    void Awake()
+    {
+        instance = this; 
+    }
+}
